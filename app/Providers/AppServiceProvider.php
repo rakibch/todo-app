d5738fda83530054cc6a18c6077e5539f598a5f2
rakibch <rakib9204@gmail.com>
2025-06-04 20:44:19 +0600

    integrated passport and created AuthController

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(60));
     }
 }
